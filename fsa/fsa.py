4c4494ac7d7cb9f0bb826e1b7349a46d18b2b679
jamespblackburn <39921110+jamespblackburn@users.noreply.github.com>
2018-08-12 20:44:22 -0700

    Now have first code for fully state-based automaton plus pretty-printing

--- fsa/fsa.py
+++ fsa/fsa.py
@@ -5,44 +5,40 @@ import json
 
 class State():
     #docstring
-    def __init__(self, id_number, exits={}, starts=False, accepts=False):
-        # exits: dict with char keys and state id_number values.
+    def __init__(self, id_number, reachable_states={}, starts=False, accepts=False):
+        
+        # reachable_states = dict, key is destination state, value is tuple (symbol, probability)
+
         self.id_number = id_number
         self.starts = starts
         self.accepts = accepts
-        self.exits = exits
+        self.reachable_states = reachable_states
     
     def __str__(self):
-        as_string = str(self.id_number)
-        as_string += " start_state={}".format(self.starts)
-        as_string += " accept_state={}".format(self.accepts)
+        as_string = str("q" + str(self.id_number))
+
+        for next_state in self.reachable_states.keys():
+            as_string += " --> " +\
+                         "q" + str(next_state) +\
+                         "\tsymbol=" +\
+                         str(self.reachable_states[next_state][0]) +\
+                         "\tprob=" +\
+                         str(self.reachable_states[next_state][1])
+                         
         return as_string
 
 
 class Automaton():
-    def __init__(self, states, transitions):
+    def __init__(self, states):
         self.state_set = set()
-        self.trans_table = []
-        self.as_string = ""
-        self.start_state = None
-        self.accept_states = set()
-
         for state in states:
             self.state_set.add(state)
-            if state.starts:
-                self.start_state = state
-            if state.accepts:
-                self.accept_states.add(state)
-        
-        for transition in transitions:
-            self.trans_table.append(transition)
 
     def __str__(self):
+        as_string = ""
         for state in self.state_set:
-            self.as_string += str(state) + "\n"
-        for transition in self.trans_table:
-            self.as_string += str(transition) + "\n"
-        return self.as_string.strip()
+            as_string += str(state) + "\n"
+        return as_string.strip()
 
     def recognize(self, input_string):
         # docstring: returns True if string is recognized, otherwise False
@@ -79,4 +75,11 @@ class Automaton():
         # after loop exits: does our current state accept?
             # If yes, return True
             # If no, return False
-            
+
+
+q0 = State(0, {1:('a', 1)}, starts=True, accepts=False)
+q1 = State(1, {2:('b', 1)}, starts=False, accepts=False)
+q2 = State(2, {3:('c', 1)}, starts=False, accepts=True)
+
+fsa = Automaton([q0, q1, q2])
+print(fsa)
