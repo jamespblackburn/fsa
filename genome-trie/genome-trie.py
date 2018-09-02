5914d12736e04048149629fcc54bd23d6defebe8
jamespblackburn <39921110+jamespblackburn@users.noreply.github.com>
2018-09-01 22:30:26 -0700

    Successful location of test sequences within test genome

--- genome-trie/genome-trie.py
+++ genome-trie/genome-trie.py
@@ -7,11 +7,10 @@ class Node():
         self.accepts = False
 
 
-def getTargets(target_file):
-    root_node = Node()
+def getTargets(target_file, root):
     with open(target_file, 'r') as f:
         for line in f:
-            buildTrie(line, root_node)
+            buildTrie(line, root)
 
 
 def buildTrie(target_string, root):
@@ -24,5 +23,24 @@ def buildTrie(target_string, root):
     current_node.accepts = True
 
 
+def traverse(input_string, root_node):
+    current_node = root_node
+    i, j = 0, 0
+    while j < len(input_string):
+        if input_string[j] in current_node.children.keys():
+            current_node = current_node.children[input_string[j]]
+            j += 1
+        else:
+            i += 1
+            j = i
+            current_node = root_node       
+        if current_node.accepts:
+            print('found sequence {}'.format(input_string[i:j]))
+                        
+
 if __name__ == "__main__":
-    getTargets('test_sequence.txt')
+    root_node = Node()
+    getTargets('test_sequence.txt', root_node)
+    with open('test_genome.txt', 'r') as genome:
+        for line in genome:
+            traverse(line, root_node)
